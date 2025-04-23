da39762573b1c5eaa8c64298dc748aa504d06fc1
tagaism <i77tag@gmail.com>
2025-04-23 23:00:51 +0900

    Week5, Payment System 2

--- java/Week5/PaymentSystem/CreditCardPayment/CreditCardPayment.java
+++ java/Week5/PaymentSystem/CreditCardPayment/CreditCardPayment.java
@@ -1,10 +1,30 @@
 package Week5.PaymentSystem.CreditCardPayment;
 
 import Week5.PaymentSystem.IPayable.IPayable;
-
+import java.util.Random;
 public class CreditCardPayment implements IPayable{
+    private double paymentAmount;
+
+    public CreditCardPayment() {
+        Random rand = new Random();
+        double paymentAmount = rand.nextInt(99) + 1;
+        SetPaymentAmount(paymentAmount);
+    }
+
+    public void SetPaymentAmount(double payment) {
+        this.paymentAmount = payment;
+    }
+
+    public double GetPaymentAmount() {
+        return this.paymentAmount;
+    }
     @Override
     public void Pay() {
-        System.out.println("Qling");
+        System.out.printf("Qling -%.2f$\n", GetPaymentAmount());
+    }
+    
+    @Override
+    public void Refund() {
+        System.out.printf("Depositing account +%.2f$\n", GetPaymentAmount());
     }
 }
