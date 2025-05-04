fba4893bcee8a63dcbe698f07dd8a1adf234a978
tagaism <i77tag@gmail.com>
2025-05-04 23:32:00 +0900

    improved firstUniqueChar with array

--- java/LeetCode/FirstUniqueCharacter.java
+++ java/LeetCode/FirstUniqueCharacter.java
@@ -12,21 +12,34 @@ public class FirstUniqueCharacter {
     }
 
     private static int solution(String str) {
-        HashMap<Character, Integer> seen = new HashMap<>();
-        for(char ch : str.toCharArray()) {
-            if(seen.containsKey(ch)) {
-                seen.put(ch, seen.get(ch) + 1);
-            } else {
-                seen.put(ch, 1);
-            }
+        // implementation with hashmap
+        // HashMap<Character, Integer> seen = new HashMap<>();
+        // for(char ch : str.toCharArray()) {
+        //     if(seen.containsKey(ch)) {
+        //         seen.put(ch, seen.get(ch) + 1);
+        //     } else {
+        //         seen.put(ch, 1);
+        //     }
+        // }
+
+        // for(int i = 0; i < str.length(); i++) {
+        //     char c = str.charAt(i);
+        //     if(seen.get(c) == 1) {
+        //         return i;
+        //     }
+        // }
+        // implementation with array
+        int[] freqArr = new int[26];
+        for(int i = 0; i < str.length(); i++) {
+            freqArr[str.charAt(i) - 'a']++;
         }
 
         for(int i = 0; i < str.length(); i++) {
-            char c = str.charAt(i);
-            if(seen.get(c) == 1) {
+            if(freqArr[str.charAt(i) - 'a'] == 1) {
                 return i;
             }
         }
+
         return -1;
     }
 }
