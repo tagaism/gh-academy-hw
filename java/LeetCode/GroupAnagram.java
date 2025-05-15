4106f0d3ecfc1b63aa988c36678fe96dff9d0e91
tagaism <i77tag@gmail.com>
2025-05-15 23:48:33 +0900

    Java LC. Grouping anagram.

--- java/LeetCode/GroupAnagram.java
+++ java/LeetCode/GroupAnagram.java
@@ -1,64 +1,41 @@
 package LeetCode;
 
-import java.util.Scanner;
-
 public class GroupAnagram {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int l = sc.nextInt();
-        String[] strs = new String[l];
-
-        for(int i = 0; i < l; i++) {
-            strs[i] = sc.nextLine();
-        }
-        sc.close();
-        
-    }
-    public List<List<String>> groupAnagrams(String[] strs) {
-        ArrayList<String> arr = new ArrayList<String>();
-        for(String s : strs) {
-            arr.add(s);
-        }
-        while(arr.size() > 0) {
-            for(int i = 0; i < arr.size() - 1; i++) {
-                if(isAnagram(arr[i], arr[i + 1])) {
-                    
-                }
-            }
-        }
+        String[] strs = new String[]{"eat","tea","tan","ate","nat","bat"};
+        groupAnagram(strs);
     }
 
-    private static boolean isAnagram(String s1, String s2) {
-        if(s1.length() != s2.length()) {
-            return false;
+    public static String[][] groupAnagram(String[] arr) {
+        for(String str : arr) {
+            for(String)
         }
-        HashMap<Character, Integer> map1 = new HashMap<>();
-        HashMap<Character, Integer> map2 = new HashMap<>();
+        return new String[][]{};
+    }
 
-        for(char c : s1.toCharArray()) {
-            if(map1.containsKey(c)) {
-                map1.put(c, map1.get(c) + 1);
+    public static boolean isAnagram(String st1, String st2) {
+        int[] freq = new int[26];
+        for(char ch : st1.toCharArray()) {
+            if(freq[ch - 'a'] != 0) {
+                freq[ch - 'a']++;
             } else {
-                map1.put(c, 1);
-            }
-        }
-        for(char c : s2.toCharArray()) {
-            if(map2.containsKey(c)) {
-                map2.put(c, map2.get(c) + 1);
-            } else {
-                map2.put(c, 1);
+                freq[ch - 'a'] = 1;
             }
         }
 
-        for(char c : map1.keySet()) {
-            if(map1.get(c) != map2.get(c)) {
+        for(char ch : st2.toCharArray()) {
+            freq[ch - 'a']--;
+        }
+
+        for(int i : freq) {
+            if(i != 0) {
                 return false;
             }
         }
-
         return true;
     }
+
 
 
 }
