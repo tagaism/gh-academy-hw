4ebbe632eedffbf973991150c256c3460c5a17be
tagaism <i77tag@gmail.com>
2025-06-03 23:37:47 +0900

    Java HM. Zuma solution!!

--- java/Week3/Balls/Balls.java
+++ java/Week3/Balls/Balls.java
@@ -1,16 +1,51 @@
-package Week3.Balls;
-
+// package Week3.Balls;
+// Aisultan solution
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Balls {
     public static void main(String[] arg) {
         Scanner sc = new Scanner(System.in);
         int l = sc.nextInt();
-        int[] arr = new int[l];
+        ArrayList<Integer> arr = new ArrayList<>();
         for(int i = 0; i < l; i++) {
-            arr[i] = sc.nextInt();
+            arr.add(sc.nextInt());
         }
         sc.close();
+
+        Stack<Integer> st = new Stack<>();
         
+        for(int i = 0; i < l; i++) {
+            st.add(arr.get(i));
+            if(st.size() < 3) {
+                continue;
+            }
+            int a = st.pop();
+            int b = st.pop();
+            int c = st.pop();
+            if(a != b || a != c) {
+                st.add(c);
+                st.add(b);
+                st.add(a);
+                continue;
+            }
+            while(i < l) {
+                if(arr.get(i) != a) {
+                    break;
+                }
+                i++;
+            }
+            i--;
+        }
+        for(int s : st) {
+            System.out.printf("%d ", s);
+        }
+        System.out.println();
+        System.out.println(l - st.size());
+        // for(int i : arr) {
+        //     System.out.printf("%d ", i);
+        // }
+        // System.out.println();
     }
 }
