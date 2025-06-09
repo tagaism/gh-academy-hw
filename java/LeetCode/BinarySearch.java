c7006bdf5ca10d25cfea0b4c04dcbcc6ad21e064
tagaism <i77tag@gmail.com>
2025-06-09 22:01:51 +0900

    LC. binary search

---
+++ java/LeetCode/BinarySearch.java
@@ -0,0 +1,24 @@
+package LeetCode;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] nums = new int[] {};
+        System.out.println(solution(nums, 6, 0, nums.length - 1));
+    }
+    private static int solution(int[] numss, int tar, int l, int r) {
+        if (l > r) {
+            return -1;
+        }
+        int m = (r + l) / 2;
+        if(numss[m] == tar) {
+            return m;
+        }
+        if(numss[m] < tar) {
+            l = m + 1;
+        }
+        if(numss[m] > tar) {
+            r = m - 1;
+        }
+        return solution(numss, tar, l, r);
+    }
+}
