ce7447024bbaab9a4af1abf67b636897cdff6eda
tagaism <i77tag@gmail.com>
2025-06-10 23:02:33 +0900

    Java LC. 2D matrix binary search. Some improvments

--- java/LeetCode/BinarySearch2D.java
+++ java/LeetCode/BinarySearch2D.java
@@ -3,9 +3,9 @@ package LeetCode;
 public class BinarySearch2D {
     public static void main(String[] args) {
         int[][] matrix = new int[][] {
-            {1,3,5,7},{10,11,16,20},{23,30,34,50}
+            {1},{10},{50}
         };
-        System.out.println(solution(matrix, 10));
+        System.out.println(solution(matrix, 20));
     }
     private static boolean solution(int[][] matrix, int target) {
         int arrIdx = getArrIdx(matrix, target, 0, matrix.length - 1);
@@ -13,21 +13,18 @@ public class BinarySearch2D {
     }
 
     private static int getArrIdx(int[][] matrix, int target, int l, int r) {
-        if(l > r) {
-            if(r < 0) {
-                return 0;
-            }
-            return (l + r) / 2;
-        }
         int m = (l + r) / 2;
-        int len = matrix[0].length - 1;
-        if(target >= matrix[m][0] && target <= matrix[m][len]) {
+        if(l > r) {
+            return r < 0 ? 0 : m;
+        }
+        int lastIdx = matrix[0].length - 1;
+        if(target >= matrix[m][0] && target <= matrix[m][lastIdx]) {
             return m;
         }
         if(target <= matrix[m][0]) {
             r = m - 1;
         }
-        if(target >= matrix[m][len]) {
+        if(target >= matrix[m][lastIdx]) {
             l = m + 1;
         }
         return getArrIdx(matrix, target, l, r);
