fb2e4e429c9fd0c860326bf970b7c276e17fb94d
tagaism <i77tag@gmail.com>
2025-06-19 06:41:28 +0900

    Java LC. Single number. XOR

--- java/LeetCode/SingleNumber.java
+++ java/LeetCode/SingleNumber.java
@@ -2,6 +2,15 @@ package LeetCode;
 
 public class SingleNumber {
     public static void main(String[] args) {
-        
+        int[] arr = {4,1,2,1,2,4,234,4,4,234,2342352};
+        System.out.println(solution(arr));
+    }
+
+    private static int solution(int[] arr) {
+        int ans = 0;
+        for(int n : arr) {
+            ans = ans ^ n;
+        }
+        return ans;
     }
 }
