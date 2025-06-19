dc9c8c3c6ece9eb5d9911ef031243afb4aabbe69
tagaism <i77tag@gmail.com>
2025-06-19 23:09:23 +0900

    Java LC. Majority element

---
+++ java/LeetCode/MajorityElement.java
@@ -0,0 +1,25 @@
+package LeetCode;
+
+import java.util.HashMap;
+
+public class MajorityElement {
+    public static void main(String[] args) {
+        System.out.println(solution(new int[]{12, 12, 12, 12, 2,2,1,1,1,2,2,4,4,4,4,4,4,4,4,4,4,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12}));
+    }
+    private static int solution(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int n : nums) {
+            if(map.containsKey(n)) {
+                map.replace(n, map.get(n)+1);
+            } else {
+                map.put(n, 1);
+            }
+        }
+        for(int n : map.keySet()) {
+            if(map.get(n) > nums.length / 2) {
+                return n;
+            }
+        }
+        return 0;
+    }
+}
