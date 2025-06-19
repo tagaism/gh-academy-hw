db7c1a99f0a3c744deb4c14ffee08451ace53ca8
tagaism <i77tag@gmail.com>
2025-06-19 23:13:44 +0900

    minor

--- java/LeetCode/MajorityElement.java
+++ java/LeetCode/MajorityElement.java
@@ -4,16 +4,17 @@ import java.util.HashMap;
 
 public class MajorityElement {
     public static void main(String[] args) {
-        System.out.println(solution(new int[]{12, 12, 12, 12, 2,2,1,1,1,2,2,4,4,4,4,4,4,4,4,4,4,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12}));
+        System.out.println(solution(new int[]{2,2,1,1,1,2,2,4,4,4,4,4,4,4,4,4,4}));
     }
     private static int solution(int[] nums) {
         HashMap<Integer, Integer> map = new HashMap<>();
         for(int n : nums) {
-            if(map.containsKey(n)) {
-                map.replace(n, map.get(n)+1);
-            } else {
-                map.put(n, 1);
-            }
+            map.put(n, map.getOrDefault(n, 1) + 1);
+            // if(map.containsKey(n)) {
+            //     map.replace(n, map.get(n)+1);
+            // } else {
+            //     map.put(n, 1);
+            // }
         }
         for(int n : map.keySet()) {
             if(map.get(n) > nums.length / 2) {
