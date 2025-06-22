d68be45a871523896e78c25781af34e3d3f08476
tagaism <i77tag@gmail.com>
2025-06-22 18:20:05 +0900

    Java LC. Reverse bits

--- java/LeetCode/ReverseBits.java
+++ java/LeetCode/ReverseBits.java
@@ -2,6 +2,24 @@ package LeetCode;
 
 public class ReverseBits {
     public static void main(String[] args) {
-        
+        System.out.println(solution(1025));
+    }
+
+    private static int solution(int n) {
+        int res = 0;
+        //                                      i
+        //0011 1001 0111 1000 0010 1001 0100 0010
+        //
+
+
+        for(int i = 0; i < 32; i++) {
+            int bit = (n >> i) & 1;
+            if(bit == 1) {
+                System.out.printf("%d ", bit);
+                res |= 1 << (31 - i);
+            }
+        }
+        System.err.println();
+        return res;
     }
 }
