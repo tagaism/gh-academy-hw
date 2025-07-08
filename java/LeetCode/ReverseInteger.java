7f329233bb3b3012381a0d55dc5c5bae0402061e
tagaism <i77tag@gmail.com>
2025-07-09 08:49:54 +0900

    minor

--- java/LeetCode/ReverseInteger.java
+++ java/LeetCode/ReverseInteger.java
@@ -2,10 +2,19 @@ package LeetCode;
 
 public class ReverseInteger {
     public static void main(String[] args) {
-
+        System.out.println(solution(1534236469));
     }
     private static int solution(int x) {
         int res = 0;
-        return res;
+        int sign = x / Math.abs(x);
+        while(x > 0) {
+            int t = x % 10;
+            res += t;
+            x /= 10;
+            if(x > 0) {
+                res *= 10;
+            }
+        }
+        return res * sign;
     }
 }
