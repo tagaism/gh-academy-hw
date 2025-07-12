a57cfdce38ca6920aa5045ff5d86d23ed3dd9eb3
tagaism <i77tag@gmail.com>
2025-07-12 23:54:42 +0900

    Java. Linked List.

--- java/LeetCode/RotateArray.java
+++ java/LeetCode/RotateArray.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class RotateArray {
     public static void main(String[] args) {
         int[] arr = new int[]{1, 2, 3, 4, 5, 6, 7};
-        rotate(arr, 3);
+        rotate(arr, 26);
         System.out.println(Arrays.toString(arr));
 
     }
