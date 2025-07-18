5f2f5561f5840f7d86594cde014a798277a40c62
tagaism <i77tag@gmail.com>
2025-07-18 23:59:47 +0600

    Java DLL. Prepend. Insert element

--- java/DoublyLinkedList/DoublyLinkedList.java
+++ java/DoublyLinkedList/DoublyLinkedList.java
@@ -45,6 +45,26 @@ public class DoublyLinkedList {
     }
 
     /**
+     * Inserts a new node with the specified value at the beginning of the doubly linked list.
+     * If the list is empty, the new node becomes both the head and the tail.
+     * Otherwise, the new node is linked before the current head.
+     *
+     * @param val the value to be added to the front of the list
+     */
+    public void prepend(int val) {
+        Node nn = new Node(val);
+        if(head == null) {
+            head = nn;
+            tail = nn;
+        } else {
+            nn.next = head;
+            head.prev = nn;
+            head = nn;
+        }
+        length++;
+    } 
+
+    /**
      * Removes and returns the last node from the doubly linked list.
      *
      * @return the removed {@code Node} if the list is not empty; {@code null} otherwise
@@ -104,10 +124,51 @@ public class DoublyLinkedList {
         return curr;
     }
 
+    /**
+     * Updates the value of the node at the specified index in the doubly linked list.
+     *
+     * @param idx the index of the node to update
+     * @param val the new value to set for the node
+     * @return true if the node was found and updated; false if the index is out of bounds
+     */
     public boolean set(int idx, int val) {
         Node temp = get(idx);
         if(temp == null) return false;
         temp.value = val;
+        return true;
+    }
+
+    /**
+     * Inserts a new node with the specified value at the given index in the doubly linked list.
+     *
+     * @param idx the position at which to insert the new node (0-based index)
+     * @param val the value to be stored in the new node
+     * @return true if the insertion was successful; false if the index is out of bounds
+     *
+     * <p>
+     * If {@code idx} is 0, the value is prepended to the list.
+     * If {@code idx} is equal to the current length, the value is appended to the list.
+     * Otherwise, the value is inserted at the specified position.
+     * </p>
+     */
+    public boolean insert(int idx, int val) {
+        if(idx < 0 || idx > length) return false;
+        if(idx == 0) {
+            prepend(val);
+            return true;
+        }
+        if(idx == length) {
+            append(val);
+            return true;
+        }
+        Node nn = new Node(val);
+        Node before = get(idx - 1);
+        Node after = get(idx + 1);
+        nn.next = after;
+        after.prev = nn;
+        nn.prev = before;
+        before.next = nn;
+        length++;
         return true;
     }
 }
