8acf0261d65b87a38d5f4818fe3665f705ce9d18
tagaism <i77tag@gmail.com>
2025-07-21 19:30:19 +0900

    Java Queue. Enqueue

--- java/Queue/Queue.java
+++ java/Queue/Queue.java
@@ -1,6 +1,40 @@
 package Queue;
 
 public class Queue {
+    Node first;
+    Node last;
+    int length;
+
+    class Node {
+        int value;
+        Node next;
+        Node(int val) {
+            value = val;
+        }
+    }
+
+    public Queue(int val) {
+        Node nn = new Node(val);
+        first = nn;
+        last = nn;
+        length++;
+    }
     
-    // public 
+    /**
+     * Adds a new element with the specified value to the end of the queue.
+     * Creates a new node and updates the queue pointers accordingly.
+     * If the queue is empty, both 'first' and 'last' will point to the new node.
+     * Increments the length of the queue.
+     *
+     * @param val the value to be added to the queue
+     */
+    public void enqueue(int val) {
+        Node nn = new Node(val);
+        nn.next = last;
+        last = nn;
+        if(first == null) {
+            first = nn;
+        }
+        length++;
+    }
 }
