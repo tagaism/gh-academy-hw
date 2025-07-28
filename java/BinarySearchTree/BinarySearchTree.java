00a738eed197b6509d91b52b34ce0c52e7e58397
tagaism <i77tag@gmail.com>
2025-07-28 23:17:35 +0900

    Java BST. insert

--- java/BinarySearchTree/BinarySearchTree.java
+++ java/BinarySearchTree/BinarySearchTree.java
@@ -7,5 +7,31 @@ public class BinarySearchTree {
         int value;
         Node left;
         Node right;
+        Node(int val) {
+            value = val;
+        }
+    }
+
+    public Node getRoot() {
+        return root;
+    }
+
+    public boolean insert(int val) {
+        Node nn = new Node(val);
+        if(root == null) {
+            root = nn;
+        } else {
+            Node curr = new Node(val);
+            while(curr != null) {
+                if(val > curr.value) {
+                    curr.right = nn;
+                    curr = curr.right;
+                } else {
+                    curr.left = nn;
+                    curr = curr.left;
+                }
+            }
+        }
+        return true;
     }
 }
