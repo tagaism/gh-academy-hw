6370fb41ef1d8e02d83f4dab24aac2ac29760acb
tagaism <i77tag@gmail.com>
2025-08-03 21:09:55 +0600

    Java LC. Group anagram

--- java/LeetCode/GroupAnagram.java
+++ java/LeetCode/GroupAnagram.java
@@ -3,8 +3,7 @@ package LeetCode;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-
+import java.util.Arrays;
 public class GroupAnagram {
 
     public static void main(String[] args) {
@@ -14,28 +13,43 @@ public class GroupAnagram {
     }
 
     public static List<List<String>> groupAnagrams(String[] strs) {
-        Map<String, List<String>> ans = new HashMap<>();
+        // Map<String, List<String>> ans = new HashMap<>();
 
-        for (String s : strs) {
-            int[] count = new int[26];
+        // for (String s : strs) {
+        //     int[] count = new int[26];
 
-            // Count frequency of each letter in the string
-            for (char c : s.toCharArray()) {
-                count[c - 'a']++;
-            }
+        //     // Count frequency of each letter in the string
+        //     for (char c : s.toCharArray()) {
+        //         count[c - 'a']++;
+        //     }
 
-            StringBuilder sb = new StringBuilder();
-            for (int num : count) {
-                sb.append(num).append("#");
+        //     StringBuilder sb = new StringBuilder();
+        //     for (int num : count) {
+        //         sb.append(num).append("#");
+        //     }
+        //     String key = sb.toString();
+        //     if (!ans.containsKey(key)) {
+        //         ans.put(key, new ArrayList<>());
+        //     }
+        //     ans.get(key).add(s);
+        // }
+
+        // return new ArrayList<>(ans.values());        
+
+        HashMap<String, ArrayList<String>> map = new HashMap<>();
+        for(String str : strs) {
+            char[] cs = str.toCharArray();
+            Arrays.sort(cs);
+            String canonical = new String(cs);
+            if(map.containsKey(canonical)) {
+                map.get(canonical).add(str);
+            } else {
+                ArrayList<String> newGr = new ArrayList<>();
+                newGr.add(str);
+                map.put(canonical, newGr);
             }
-            String key = sb.toString();
-            if (!ans.containsKey(key)) {
-                ans.put(key, new ArrayList<>());
-            }
-            ans.get(key).add(s);
         }
-
-        return new ArrayList<>(ans.values());        
+        return new ArrayList<>(map.values());
     }
 
     public static String[][] groupAnagram(String[] arr) {
