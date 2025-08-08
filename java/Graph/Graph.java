f71c20957be9f00a34f3b0c9ae4769d8df3c8288
tagaism <i77tag@gmail.com>
2025-08-08 23:58:59 +0900

    minor

--- java/Graph/Graph.java
+++ java/Graph/Graph.java
@@ -3,13 +3,25 @@ package Graph;
 import java.util.HashMap;
 import java.util.ArrayList;
 
+/**
+ * This class represents a Graph data structure.
+ * It uses an adjacency list to store the graph.
+ */
 public class Graph {
     private HashMap<String, ArrayList<String>> adjList = new HashMap<>();
 
+    /**
+     * Prints the adjacency list representation of the graph.
+     */
     public void printGraph() {
         System.out.println(adjList);
     }
 
+    /**
+     * Adds a new vertex to the graph.
+     * @param vertex The vertex to be added.
+     * @return true if the vertex was added successfully, false otherwise (if the vertex already exists).
+     */
     public boolean addVertex(String vertex) {
         if(adjList.get(vertex) == null) {
             adjList.put(vertex, new ArrayList<>());
@@ -18,6 +30,12 @@ public class Graph {
         return false;
     }
 
+    /**
+     * Adds an edge between two vertices in the graph.
+     * @param vertex1 The first vertex.
+     * @param vertex2 The second vertex.
+     * @return true if the edge was added successfully, false otherwise (if one or both vertices do not exist).
+     */
     public boolean addEdge(String vertex1, String vertex2) {
         if(adjList.get(vertex1) != null && adjList.get(vertex2) != null) {
             adjList.get(vertex1).add(vertex2);
@@ -27,12 +45,18 @@ public class Graph {
         return false;
     }
 
+    /**
+     * Removes an edge between two vertices in the graph.
+     * @param vertex1 The first vertex.
+     * @param vertex2 The second vertex.
+     * @return true if the edge was removed successfully, false otherwise (if one or both vertices do not exist).
+     */
     public boolean removeEdge(String vertex1, String vertex2) {
-    if(adjList.get(vertex1) != null && adjList.get(vertex2) != null) {
-        adjList.get(vertex1).remove(vertex2);
-        adjList.get(vertex2).remove(vertex1);
-        return true;  
-    }
-    return false;
+        if(adjList.get(vertex1) != null && adjList.get(vertex2) != null) {
+            adjList.get(vertex1).remove(vertex2);
+            adjList.get(vertex2).remove(vertex1);
+            return true;  
+        }
+        return false;
     }
 }
