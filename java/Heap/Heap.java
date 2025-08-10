b310b788d0b38911626a00153cb758999c4f54c7
tagaism <i77tag@gmail.com>
2025-08-10 21:08:26 +0600

    Java Heap. Initialization

--- java/Heap/Heap.java
+++ java/Heap/Heap.java
@@ -1,5 +1,15 @@
 package java.Heap;
 
+import java.util.ArrayList;
+
 public class Heap {
+    private ArrayList<Integer> heap;
     
+    public Heap() {
+        heap = new ArrayList<>();
+    }
+
+    public ArrayList<Integer> getHeap() {
+        return new ArrayList<>(heap);
+    }
 }
