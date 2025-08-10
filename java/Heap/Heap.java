cabef2616f60d47b07785fa97a6d4ac66075112f
tagaism <i77tag@gmail.com>
2025-08-11 02:39:31 +0900

    minor

--- java/Heap/Heap.java
+++ java/Heap/Heap.java
@@ -1,7 +1,15 @@
 package java.Heap;
 
 import java.util.ArrayList;
-
+/**
+ * Implements a Min-Heap data structure using an `ArrayList`.
+ * This class provides basic heap operations such as insertion,
+ * and allows retrieval of the heap's current state.
+ * <p>
+ * The heap property is maintained such that the value of each node
+ * is less than or equal to the value of its children.
+ * </p>
+ */
 public class Heap {
     private ArrayList<Integer> heap;
     
