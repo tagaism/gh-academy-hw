c976ceb450f66107a27db317757466b049ee4f40
tagaism <tagai.ismailov@gmail.com>
2025-08-18 23:20:20 +0900

    Java LC. Product Array Except self (boilerplate)

---
+++ java/LeetCode/ProductArrayExceptSelf.java
@@ -0,0 +1,16 @@
+package LeetCode;
+
+public class ProductArrayExceptSelf {
+    public static void main(String[] arsgs) {
+
+    }
+
+    private int[] solution(int[] nums) {
+        int n = nums.length;
+
+
+
+        int[] ans = new int[n];
+        return ans;
+    }
+}
