a3fe4746f23648214e885aeb605b84ac4410a7c2
tagaism <tagai.ismailov@gmail.com>
2025-09-11 23:56:59 +0900

    minor

--- java/LeetCode/LinkedListCicle.java
+++ java/LeetCode/LinkedListCicle.java
@@ -2,6 +2,11 @@ package LeetCode;
 
 import LinkedList.LinkedList;
 
+/**
+ * This class provides a solution to detect a cycle in a linked list.
+ * The `solution` method uses the fast and slow pointer approach (Floyd's Cycle-Finding Algorithm)
+ * to determine if a cycle exists in the given linked list.
+ */
 public class LinkedListCicle {
     public static void main(String[] args) {
 
