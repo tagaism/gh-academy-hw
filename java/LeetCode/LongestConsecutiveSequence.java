ca42698e6049bfeb3a7d7bf9bf31cc1b01a1eafd
tagaism <tagai.ismailov@gmail.com>
2025-10-21 21:00:50 +0600

    minor

--- java/LeetCode/LongestConsecutiveSequence.java
+++ java/LeetCode/LongestConsecutiveSequence.java
@@ -1,5 +1,14 @@
 package LeetCode;
 
 public class LongestConsecutiveSequence {
-    
+  public int solution(int[] nums) {
+    Set<Integer> set = new HashSet<>();
+    for(int n : nums) {
+      set.add(n); 
+     }
+    int theLargest = 0
+    for(int n : set) {
+     a
+    }
+  }    
 }
